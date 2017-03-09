af26a6070f730d98f5789f25ab82b20f460d4141
Guenter Roeck <linux@roeck-us.net>
2017-03-09 15:39:37 +0200

    usb: host: xhci-plat: Fix timeout on removal of hot pluggable xhci controllers
    commit dcc7620cad5ad1326a78f4031a7bf4f0e5b42984 upstream.
    
    Upstream commit 98d74f9ceaef ("xhci: fix 10 second timeout on removal of
    PCI hotpluggable xhci controllers") fixes a problem with hot pluggable PCI
    xhci controllers which can result in excessive timeouts, to the point where
    the system reports a deadlock.
    
    The same problem is seen with hot pluggable xhci controllers using the
    xhci-plat driver, such as the driver used for Type-C ports on rk3399.
    Similar to hot-pluggable PCI controllers, the driver for this chip
    removes the xhci controller from the system when the Type-C cable is
    disconnected.
    
    The solution for PCI devices works just as well for non-PCI devices
    and avoids the problem.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -205,7 +205,11 @@ static int xhci_plat_remove(struct platform_device *dev)
 	struct usb_hcd	*hcd = platform_get_drvdata(dev);
 	struct xhci_hcd	*xhci = hcd_to_xhci(hcd);
 
+<<<<<<< HEAD
 	pm_runtime_disable(&dev->dev);
+=======
+	/*xhci->xhc_state |= XHCI_STATE_REMOVING;*/
+>>>>>>> a3fdfc6... usb: host: xhci-plat: Fix timeout on removal of hot pluggable xhci controllers
 
 	usb_remove_hcd(xhci->shared_hcd);
 	usb_put_hcd(xhci->shared_hcd);
