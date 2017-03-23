12bb73d9ccfc2cf82e285396a495884b7ef18615
Andy Whitcroft <apw@canonical.com>
2017-03-23 07:45:44 +0000

    xfrm_user: validate XFRM_MSG_NEWAE incoming ESN size harder
    commit f843ee6dd019bcece3e74e76ad9df0155655d0df upstream.
    
    Kees Cook has pointed out that xfrm_replay_state_esn_len() is subject to
    wrapping issues.  To ensure we are correctly ensuring that the two ESN
    structures are the same size compare both the overall size as reported
    by xfrm_replay_state_esn_len() and the internal length are the same.
    
    CVE-2017-7184
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Acked-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- net/xfrm/xfrm_user.c
+++ net/xfrm/xfrm_user.c
@@ -391,6 +391,7 @@ static inline int xfrm_replay_verify_len(struct xfrm_replay_state_esn *replay_es
 	ulen = xfrm_replay_state_esn_len(up);
 
 	/* Check the overall length and the internal bitmap length to avoid
+<<<<<<< HEAD
 	 * potential overflow.
          */
 	if (nla_len(rp) < ulen ||
@@ -399,6 +400,12 @@ static inline int xfrm_replay_verify_len(struct xfrm_replay_state_esn *replay_es
 		return -EINVAL;
 
 	if (up->replay_window > up->bmp_len * sizeof(__u32) * 8)
+=======
+/*	 * potential overflow. *//*
+	if (nla_len(rp) < ulen ||
+	    xfrm_replay_state_esn_len(replay_esn) != ulen ||
+	    replay_esn->bmp_len != up->bmp_len)*/
+>>>>>>> cbd95d1... xfrm_user: validate XFRM_MSG_NEWAE incoming ESN size harder
 		return -EINVAL;
 
 	if (up->replay_window > up->bmp_len * sizeof(__u32) * 8)
